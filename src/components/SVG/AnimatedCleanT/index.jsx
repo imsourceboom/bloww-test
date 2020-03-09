48b1ac041a3f5b77dec46a470b6af0c883c8e2f5
imsourceboom <ecmascript.developer@gmail.com>
2020-03-09 14:25:20 +0900

    revise cleanT animateSVG

--- src/components/SVG/AnimatedCleanT/index.jsx
+++ src/components/SVG/AnimatedCleanT/index.jsx
@@ -1,35 +1,65 @@
 import React from 'react';
 
 export default () => (
+  // <svg
+  //   xmlns="http://www.w3.org/2000/svg"
+  //   // width="797.402"
+  //   // height="279.692"
+  //   viewBox="0 0 797.402 279.692">
+  //   <g transform="translate(0 -589.332)">
+  //     <g transform="translate(0 -10)">
+  //       <path
+  //         d="M14482.189,2945H13804V2810h769v37.955l-2.653-.589c-.033.191-85.942,72.6-86.81,73.332l-1.349,24.3Z"
+  //         transform="translate(-13804 -2205.47)"
+  //         fill="#fff"
+  //       />
+  //       <g transform="translate(-73.26 -52.526)">
+  //         <path
+  //           d="M631.5,666.751,743.635,778.885,856.52,666"
+  //           fill="none"
+  //           stroke="#6fb1b9"
+  //           strokeMiterlimit="10"
+  //           strokeWidth="40"
+  //         />
+  //         <line
+  //           y2="152.515"
+  //           transform="translate(744.085 779.035)"
+  //           fill="#fff"
+  //           stroke="#6fb1b9"
+  //           strokeMiterlimit="10"
+  //           strokeWidth="40"
+  //         />
+  //       </g>
+  //     </g>
+  //   </g>
+  // </svg>
   <svg
     xmlns="http://www.w3.org/2000/svg"
     // width="797.402"
     // height="279.692"
     viewBox="0 0 797.402 279.692">
-    <g transform="translate(0 -589.332)">
-      <g transform="translate(0 -10)">
+    <g transform="translate(0 -584.332)">
+      <path
+        d="M-13804-1957.67h769.281V-1954l-97.942,96.457c.049.024-22.49,29.268-22.49,29.268l3.929,5.773.106,4.475h-652.777Z"
+        transform="translate(13804 2543)"
+        fill="#fff"
+      />
+      <g transform="translate(-73.26 -67.526)">
         <path
-          d="M14482.189,2945H13804V2810h769v37.955l-2.653-.589c-.033.191-85.942,72.6-86.81,73.332l-1.349,24.3Z"
-          transform="translate(-13804 -2205.47)"
-          fill="#fff"
+          d="M631.5,666.751,743.635,778.885,856.52,666"
+          fill="none"
+          stroke="#6fb1b9"
+          stroke-miterlimit="10"
+          stroke-width="40"
         />
-        <g transform="translate(-73.26 -52.526)">
-          <path
-            d="M631.5,666.751,743.635,778.885,856.52,666"
-            fill="none"
-            stroke="#6fb1b9"
-            strokeMiterlimit="10"
-            strokeWidth="40"
-          />
-          <line
-            y2="152.515"
-            transform="translate(744.085 779.035)"
-            fill="#fff"
-            stroke="#6fb1b9"
-            strokeMiterlimit="10"
-            strokeWidth="40"
-          />
-        </g>
+        <line
+          y2="152.515"
+          transform="translate(744.085 779.035)"
+          fill="#fff"
+          stroke="#6fb1b9"
+          stroke-miterlimit="10"
+          stroke-width="40"
+        />
       </g>
     </g>
   </svg>
