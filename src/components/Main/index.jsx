0773b38d331fc88e4d76746e72fd29a32e30d43e
imsourceboom <ecmascript.developer@gmail.com>
2020-03-12 10:21:31 +0900

    added mov file

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -76,7 +76,8 @@ export default () => {
                 loop={true}
                 playsInline={true}
                 poster="/images/main/bg/reed.jpg">
-                <source src="/images/main/bg/reed.mp4" type="video/mp4" />
+                {/* <source src="/images/main/bg/reed.mp4" type="video/mp4" /> */}
+                <source src="/images/main/bg/reed.mov" type="video/mp4" />
                 {/* <source src="/images/main/bg/reed.webm" type="video/webm" />
                 <source src="/images/main/bg/reed.ogv" type="video/ogg" /> */}
               </video>
