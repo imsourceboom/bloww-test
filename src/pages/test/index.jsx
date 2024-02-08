668338de9777cc0453d6378c545862750af205b4
imsourceboom <ecmascript.developer@gmail.com>
2024-02-08 12:45:03 +0900

    referral

---
+++ src/pages/test/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import { useDispatch } from 'react-redux';
+
+// import AboutComponent from 'src/components/About';
+
+// import SEO from 'src/templates/seo';
+// import Layout from 'src/templates/Layout';
+
+// import { notIndexAction } from 'src/state/nav';
+
+var Test = () => {
+  // var dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(notIndexAction(true));
+  // }, [dispatch]);
+
+  return (
+    <div>
+      <a href="https://t.me/notcoin_bot?start=rp_14459282">
+        https://t.me/notcoin_bot?start=rp_14459282
+      </a>
+    </div>
+  );
+};
+
+export default Test;
